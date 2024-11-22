e481791e3d9fbf34cde239ff06a2bba85cdd3bb7
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2024-11-22 12:06:47 -0700

    improved career timeline

--- components/CustomizedTimeline.tsx
+++ components/CustomizedTimeline.tsx
@@ -6,10 +6,6 @@ import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
 import TimelineDot from '@mui/lab/TimelineDot';
-import FastfoodIcon from '@mui/icons-material/Fastfood';
-import LaptopMacIcon from '@mui/icons-material/LaptopMac';
-import HotelIcon from '@mui/icons-material/Hotel';
-import RepeatIcon from '@mui/icons-material/Repeat';
 import Typography from '@mui/material/Typography';
 
 export default function CustomizedTimeline() {
@@ -22,37 +18,51 @@ export default function CustomizedTimeline() {
           variant="body2"
           color="text.secondary"
         >
-          9:30 am
+          Aug 2024 - Present
         </TimelineOppositeContent>
         <TimelineSeparator>
-          <TimelineConnector />
-          <TimelineDot>
-            <FastfoodIcon />
+          <TimelineConnector sx={{ height: '5px' }} />
+          <TimelineDot sx={{ p: 0, overflow: 'hidden', width: 50, height: 50 }}>
+            <img
+              src="asu.png"
+              alt="Custom Icon"
+              style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: '50%' }}
+            />
           </TimelineDot>
-          <TimelineConnector />
+          <TimelineConnector sx={{ height: '5px' }} />
         </TimelineSeparator>
         <TimelineContent sx={{ py: '12px', px: 2 }}>
           <Typography variant="h6" component="span">
             Graduate Teaching Assistant (CSE 578)
           </Typography>
           <Typography>Arizona State University</Typography>
-          <Typography>Aug 2024 - Present</Typography>
         </TimelineContent>
       </TimelineItem>
         <TimelineItem>
+        <TimelineOppositeContent
+          sx={{ m: 'auto 0' }}
+          align="right"
+          variant="body2"
+          color="text.secondary"
+        >
+          May 2024 - Aug 2024
+        </TimelineOppositeContent>
         <TimelineSeparator>
           <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
-          <TimelineDot color="secondary">
-            <RepeatIcon />
+          <TimelineDot sx={{ p: 0, overflow: 'hidden', width: 50, height: 50 }}>
+            <img
+              src="resilience.jpg"
+              alt="Custom Icon"
+              style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: '50%' }}
+            />
           </TimelineDot>
-          <TimelineConnector />
+          <TimelineConnector sx={{ height: '5px' }} />
         </TimelineSeparator>
         <TimelineContent sx={{ py: '12px', px: 2 }}>
           <Typography variant="h6" component="span">
             AI/ML Developer Intern
           </Typography>
           <Typography>Resilience Inc.</Typography>
-          <Typography>May 2024 - Aug 2024</Typography>
         </TimelineContent>
       </TimelineItem>
       <TimelineItem>
@@ -62,21 +72,24 @@ export default function CustomizedTimeline() {
           variant="body2"
           color="text.secondary"
         >
-          9:30 am
+          Aug 2023 - Present
         </TimelineOppositeContent>
         <TimelineSeparator>
-          <TimelineConnector />
-          <TimelineDot>
-            <FastfoodIcon />
+          <TimelineConnector sx={{ height: '5px' }} />
+          <TimelineDot sx={{ p: 0, overflow: 'hidden', width: 50, height: 50 }}>
+            <img
+              src="asu.png"
+              alt="Custom Icon"
+              style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: '50%' }}
+            />
           </TimelineDot>
-          <TimelineConnector />
+          <TimelineConnector sx={{ height: '5px' }} />
         </TimelineSeparator>
         <TimelineContent sx={{ py: '12px', px: 2 }}>
           <Typography variant="h6" component="span">
             MS CS
           </Typography>
           <Typography>Arizona State University</Typography>
-          <Typography>Aug 2023 - Present</Typography>
         </TimelineContent>
       </TimelineItem>
       <TimelineItem>
@@ -85,28 +98,42 @@ export default function CustomizedTimeline() {
           variant="body2"
           color="text.secondary"
         >
-          10:00 am
+          Jun 2021 - Feb 2023
         </TimelineOppositeContent>
         <TimelineSeparator>
-          <TimelineConnector />
-          <TimelineDot color="primary">
-            <LaptopMacIcon />
+          <TimelineConnector sx={{ height: '5px' }} />
+          <TimelineDot sx={{ p: 0, overflow: 'hidden', width: 50, height: 50 }}>
+            <img
+              src="jio.png"
+              alt="Custom Icon"
+              style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: '50%' }}
+            />
           </TimelineDot>
-          <TimelineConnector />
+          <TimelineConnector sx={{ height: '5px' }} />
         </TimelineSeparator>
         <TimelineContent sx={{ py: '12px', px: 2 }}>
           <Typography variant="h6" component="span">
             Software Enginner
           </Typography>
           <Typography>Jio Platforms Ltd.</Typography>
-          <Typography>Jun 2021 - Feb 2023</Typography>
         </TimelineContent>
       </TimelineItem>
       <TimelineItem>
+      <TimelineOppositeContent
+          sx={{ m: 'auto 0' }}
+          variant="body2"
+          color="text.secondary"
+        >
+          Jan 2021 - May 2021
+        </TimelineOppositeContent>
         <TimelineSeparator>
-          <TimelineConnector />
-          <TimelineDot color="primary" variant="outlined">
-            <HotelIcon />
+          <TimelineConnector sx={{ height: '5px' }} />
+          <TimelineDot sx={{ p: 0, overflow: 'hidden', width: 50, height: 50 }}>
+            <img
+              src="cognizant.jpg"
+              alt="Custom Icon"
+              style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: '50%' }}
+            />
           </TimelineDot>
           <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
         </TimelineSeparator>
@@ -115,39 +142,58 @@ export default function CustomizedTimeline() {
             Application Development Intern
           </Typography>
           <Typography>Cognizant Technology Solutions</Typography>
-          <Typography>Jan 2021 - May 2021</Typography>
         </TimelineContent>
       </TimelineItem>
       <TimelineItem>
+      <TimelineOppositeContent
+          sx={{ m: 'auto 0' }}
+          variant="body2"
+          color="text.secondary"
+        >
+          Jun 2018 - Aug 2018
+        </TimelineOppositeContent>
         <TimelineSeparator>
           <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
-          <TimelineDot color="secondary">
-            <RepeatIcon />
+          <TimelineDot sx={{ p: 0, overflow: 'hidden', width: 50, height: 50 }}>
+            <img
+              src="drdo.png"
+              alt="Custom Icon"
+              style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: '50%' }}
+            />
           </TimelineDot>
-          <TimelineConnector />
+          <TimelineConnector sx={{ height: '5px' }} />
         </TimelineSeparator>
         <TimelineContent sx={{ py: '12px', px: 2 }}>
           <Typography variant="h6" component="span">
             Summer Intern
           </Typography>
           <Typography>Defence Research & Development Organisation</Typography>
-          <Typography>June 2018 - Aug 2018</Typography>
         </TimelineContent>
       </TimelineItem>
       <TimelineItem>
+      <TimelineOppositeContent
+          sx={{ m: 'auto 0' }}
+          variant="body2"
+          color="text.secondary"
+        >
+          Jul 2017 - May 2021
+        </TimelineOppositeContent>
         <TimelineSeparator>
           <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
-          <TimelineDot color="secondary">
-            <RepeatIcon />
+          <TimelineDot sx={{ p: 0, overflow: 'hidden', width: 50, height: 50 }}>
+            <img
+              src="srm.png"
+              alt="Custom Icon"
+              style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: '50%' }}
+            />
           </TimelineDot>
-          <TimelineConnector />
+          <TimelineConnector sx={{ height: '5px' }} />
         </TimelineSeparator>
         <TimelineContent sx={{ py: '12px', px: 2 }}>
           <Typography variant="h6" component="span">
             B.Tech
           </Typography>
           <Typography>SRM Institute of Science and Technology (India)</Typography>
-          <Typography>July 2017 - May 2021</Typography>
         </TimelineContent>
       </TimelineItem>
     </Timeline>
