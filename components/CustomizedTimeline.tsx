4640fd2986cfe26229e26509d41126b125f655d8
Utkarsh Singh <44597899+utkarshsingh26@users.noreply.github.com>
2024-11-22 00:32:09 -0700

    adding a demo timeline

---
+++ components/CustomizedTimeline.tsx
@@ -0,0 +1,155 @@
+import * as React from 'react';
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import FastfoodIcon from '@mui/icons-material/Fastfood';
+import LaptopMacIcon from '@mui/icons-material/LaptopMac';
+import HotelIcon from '@mui/icons-material/Hotel';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import Typography from '@mui/material/Typography';
+
+export default function CustomizedTimeline() {
+  return (
+    <Timeline position="alternate">
+              <TimelineItem>
+        <TimelineOppositeContent
+          sx={{ m: 'auto 0' }}
+          align="right"
+          variant="body2"
+          color="text.secondary"
+        >
+          9:30 am
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineConnector />
+          <TimelineDot>
+            <FastfoodIcon />
+          </TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent sx={{ py: '12px', px: 2 }}>
+          <Typography variant="h6" component="span">
+            Graduate Teaching Assistant (CSE 578)
+          </Typography>
+          <Typography>Arizona State University</Typography>
+          <Typography>Aug 2024 - Present</Typography>
+        </TimelineContent>
+      </TimelineItem>
+        <TimelineItem>
+        <TimelineSeparator>
+          <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+          <TimelineDot color="secondary">
+            <RepeatIcon />
+          </TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent sx={{ py: '12px', px: 2 }}>
+          <Typography variant="h6" component="span">
+            AI/ML Developer Intern
+          </Typography>
+          <Typography>Resilience Inc.</Typography>
+          <Typography>May 2024 - Aug 2024</Typography>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent
+          sx={{ m: 'auto 0' }}
+          align="right"
+          variant="body2"
+          color="text.secondary"
+        >
+          9:30 am
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineConnector />
+          <TimelineDot>
+            <FastfoodIcon />
+          </TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent sx={{ py: '12px', px: 2 }}>
+          <Typography variant="h6" component="span">
+            MS CS
+          </Typography>
+          <Typography>Arizona State University</Typography>
+          <Typography>Aug 2023 - Present</Typography>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent
+          sx={{ m: 'auto 0' }}
+          variant="body2"
+          color="text.secondary"
+        >
+          10:00 am
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineConnector />
+          <TimelineDot color="primary">
+            <LaptopMacIcon />
+          </TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent sx={{ py: '12px', px: 2 }}>
+          <Typography variant="h6" component="span">
+            Software Enginner
+          </Typography>
+          <Typography>Jio Platforms Ltd.</Typography>
+          <Typography>Jun 2021 - Feb 2023</Typography>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineConnector />
+          <TimelineDot color="primary" variant="outlined">
+            <HotelIcon />
+          </TimelineDot>
+          <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+        </TimelineSeparator>
+        <TimelineContent sx={{ py: '12px', px: 2 }}>
+          <Typography variant="h6" component="span">
+            Application Development Intern
+          </Typography>
+          <Typography>Cognizant Technology Solutions</Typography>
+          <Typography>Jan 2021 - May 2021</Typography>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+          <TimelineDot color="secondary">
+            <RepeatIcon />
+          </TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent sx={{ py: '12px', px: 2 }}>
+          <Typography variant="h6" component="span">
+            Summer Intern
+          </Typography>
+          <Typography>Defence Research & Development Organisation</Typography>
+          <Typography>June 2018 - Aug 2018</Typography>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+          <TimelineDot color="secondary">
+            <RepeatIcon />
+          </TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent sx={{ py: '12px', px: 2 }}>
+          <Typography variant="h6" component="span">
+            B.Tech
+          </Typography>
+          <Typography>SRM Institute of Science and Technology (India)</Typography>
+          <Typography>July 2017 - May 2021</Typography>
+        </TimelineContent>
+      </TimelineItem>
+    </Timeline>
+  );
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import ResponsiveAppBar from '../components/ResponsiveAppBar';
+import CustomizedTimeline from '../components/CustomizedTimeline'
 
 function App() {
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
     <ResponsiveAppBar/>
+    <CustomizedTimeline/>
     </>
   )
 }
